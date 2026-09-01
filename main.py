bd1ff42c930dc82b8f29d0409cdf4cfede21a921
romawka <dreval.roman@gmail.com>
2026-09-01 19:01:04 +0200

    parse job title, company, location and link from listings

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello, World!")
+from bs4 import BeautifulSoup
+import requests
+
+BASE_URL = 'https://junior.guru/jobs/praha/'
+
+r = requests.get(BASE_URL)
+
+with open("page.html", 'w', encoding="utf-8") as f:
+    f.write(r.text)
+
+with open("page.html", 'r', encoding="utf-8") as f:
+    soup = BeautifulSoup(f, 'html.parser')
+
+for job in soup.find_all("div", class_="jobs-body", limit=10):
+    title = job.select_one("h3.jobs-title a.jobs-title-link").get_text(strip=True)
+    company = job.select_one("p.jobs-info strong.jobs-info-item").get_text(strip=True)
+    location = job.select_one("p.jobs-info span.jobs-info-item").get_text(strip=True)
+    url = job.select_one("h3.jobs-title a.jobs-title-link")["href"]
+
+    print(f"{title}")
+    print(f"  Company:  {company}")
+    print(f"  Location: {location}")
+    print(f"  URL:      {url}")
+    print()
